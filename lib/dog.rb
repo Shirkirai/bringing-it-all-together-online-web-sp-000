783905a0e8c3d9cecaf2aa4c5c03b3b8191294bd
Jung Lee <lee.jung.s95@gmail.com>
2020-10-13 01:35:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,14 +27,13 @@ class Dog
     SQL
     DB[:conn].execute(sql)
   end
-  
+
   def self.new_from_db(row)
     #binding.pry
     id = row[0]
     name = row[1]
     breed = row[2]
     self.new(id, name, breed)
-
   end
 
   def save
