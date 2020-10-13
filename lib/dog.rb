5d4af9d7da31597f316d7e6902719eec7a156525
Jung Lee <lee.jung.s95@gmail.com>
2020-10-13 01:22:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Dog
   attr_accessor :id, :name, :breed
 
@@ -17,6 +19,40 @@ class Dog
     SQL
 
     DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?, ?)
+    SQL
+    #binding.pry
+    saved_dog = DB[:conn].execute(sql, self.name, self.breed)
+    #binding.pry
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ?, id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def self.create(name:, breed:)
+    new_dog = Dog.new(name, breed)
+    new_dog.save
+    new_dog
+  end
+
+  def self.new_from_db
+
   end
 
 end
