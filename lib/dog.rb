60aa56dbf6286428b21304da84f000b9d6536c2a
Jung Lee <lee.jung.s95@gmail.com>
2020-10-13 02:29:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,7 +91,7 @@ class Dog
     SELECT * FROM dogs WHERE (name = ?, breed = ?)
     SQL
 
-    dog = DB[:conn].execute(sql, name, breed).first
+    dog = DB[:conn].execute(sql, self.name, self.breed).first
 
 
 
