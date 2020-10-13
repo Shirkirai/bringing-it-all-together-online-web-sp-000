e30db8297869c7fe0446dce80760ba66c048cf03
Jung Lee <lee.jung.s95@gmail.com>
2020-10-13 01:48:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,20 +50,20 @@ class Dog
 
   end
 
-  #def save
-  #  if self.id
-  #    self.update
-    #else
-      #sql = <<-SQL
-      #INSERT INTO dogs (name, breed)
-      #VALUES (?, ?)
-      #SQL
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
     #binding.pry
-  #    saved_dog = DB[:conn].execute(sql, self.name, self.breed)
+      saved_dog = DB[:conn].execute(sql, self.name, self.breed)
     #binding.pry
-  #    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 
-  #  end
+    end
 
   #end
 
